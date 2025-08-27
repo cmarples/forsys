7fc05c7dbb52f98b15da207d660e141316b49d42
Callum Marples <cmarples@hotmail.co.uk>
2025-08-27 12:46:10 +0100

    Chaste reader now creates edge and cell dictionaries, including ground truth line tensions

--- forsys/chaste.py
+++ forsys/chaste.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
-import pyvista as pv
 from typing import Tuple
+import csv
+import pyvista as pv
+import os
 
 import forsys.vertex as vertex
 import forsys.edge as edge
@@ -10,9 +12,12 @@ import forsys.cell as cell
 class Chaste:
     """Class interface with Chaste-generated files
     
+    :param fname: Path to the input Chaste directory
+    :type fname: str
     :param fname: Path to the Chaste file to read
     :type fname: str
     """
+    dirname: str
     fname: str
     
     def __post_init__(self):
@@ -28,14 +33,39 @@ class Chaste:
         """
         
         vertices = {}
-        cells = {}
         edges  = {}
+        cells = {}
         vertex_list, edge_list, face_list = self.read_vtu_file()
+        ground_truth_tensions = self.read_line_tensions()
         
         # build vertex dictionary
         for i in range(len(vertex_list)):
             vertices[i] = vertex.Vertex(i, vertex_list[i][0], vertex_list[i][1])
+            
+        # build edge dictionary
+        for i in range(len(edge_list)):
+            vertex_1 = vertices[edge_list[i][0]]
+            vertex_2 = vertices[edge_list[i][1]]
+            edges[i] = edge.SmallEdge(i, vertex_1, vertex_2)
+            edges[i].gt = ground_truth_tensions[(edge_list[i][0], edge_list[i][1])]
+            
+        # build cell dictionary
+        for i in range(len(face_list)):
+            face = face_list[i]
+            vertices_in_cell = []
+            for v in face:
+                vertices_in_cell.append(vertices[v])
+            cells[i] = cell.Cell(i, vertices_in_cell)
         
+    
+        #for _, r in edges_temp.iterrows():
+        #    edges[int(r.id)] = edge.SmallEdge(int(r.id), vertices[int(r.id1)], vertices[int(r.id2)])
+        #    edges[int(r.id)].gt = round(edges_temp.loc[edges_temp['id'] == int(r.id)]['force'].iloc[0], 4)
+        
+        #for _, r in self.get_cells().iterrows():
+        #    vlist = [edges[abs(e)].v1 if e > 0 else edges[abs(e)].v2 for e in r.edges]
+        #    gt_pressure = round(r["pressures"], 4)
+        #    cells[int(r.id)] = cell.Cell(int(r.id), vlist, gt_pressure=gt_pressure)
             
         
         return vertices, edges, cells
@@ -68,7 +98,26 @@ class Chaste:
         
         return vertex_list, edge_list, face_list
     
+    def read_line_tensions(self):
+        """
+        Obtain ground truth line tensions from file.
 
+        :return: Dictionary with vertex pairs as keys and tensions as values
+        :rtype: Dict
+        """
+        
+        line_tensions = {}
+        
+        # Attempt to read the line tension file
+        tension_file_path = os.path.join(self.dirname, "SamplingLineTensions.csv")
+        with open(tension_file_path, "r") as file:
+            reader = csv.reader(file)
+            edge_data = [(int(row[0]), int(row[1]), float(row[2])) for row in reader]
+            
+            for row in edge_data:
+                line_tensions[(row[0], row[1])] = row[2]
+        
+        return line_tensions
         
     
     
